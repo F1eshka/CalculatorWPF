f4cf50ed42c7d641444c69461f94c064722b74b4
F1eshka <144143888+F1eshka@users.noreply.github.com>
2025-01-05 18:17:50 +0300

    Add files via upload

---
+++ Calculator Wpf/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculator_Wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DeleteLastCharacter(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(ResultTextBox.Text))
+            {
+           
+                ResultTextBox.Text = ResultTextBox.Text.Remove(ResultTextBox.Text.Length - 1);
+            }
+        }
+    }
+}
